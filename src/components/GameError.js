7d018ea166ffd330b296df34b35537afc83c2911
margaret <mmoseyee@gmail.com>
2018-07-16 16:59:30 -0500

    Updates

--- src/components/GameError.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function GameError()
-{
-    return (
-        <h1>Unfortunately, no cards were found for the game. Please try again or check back later.</h1>
-    )
-}

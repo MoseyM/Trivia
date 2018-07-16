3d7ee792bbfd70302f7ad99329f062e6c886555c
margaret <mmoseyee@gmail.com>
2018-07-16 16:29:37 -0500

    1. Used `create-react-app` 2. Minor styling   - Updated the color of the header to a light purple   - Added some classes to help with minor positioning on some of the   divs 3. Created Components   - _Card_ formats the look of each trivia question   - _Game_ controls the iteration through the questions and stores the   questions within its state   - _Results_ shows the results of the users attempt at the questions as   well as offer an opportunity to replay the game   - _GameError_ added to prevent error in case retrieving questions   through the api fails. It just shows a minimally styled page with a   message.

---
+++ src/components/GameError.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function GameError()
+{
+    return (
+        <h1>Unfortunately, no cards were found for the game. Please try again or check back later.</h1>
+    )
+}
